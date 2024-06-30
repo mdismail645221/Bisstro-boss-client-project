ac587bfa5d5e6f791849dac513ae46094c4be5de
Ismail Hossain <mdismail645221@gmail.com>
2024-06-30 20:38:38 +0600

    added the footer component to the home page

--- src/Components/SectionTitle.jsx
+++ src/Components/SectionTitle.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const SectionTitle = ({ subHeading, heading }) => {
   return (
     <div className="section-title-wrapper mb-4">
-      <div className="section-wrapper-container w-[350px] m-auto">
+      <div className="section-wrapper-container w-[400px] m-auto">
         <div className="section-title-row  flex flex-col gap-3 justify-center items-center">
           <div className="section-subHeading-title">
             {subHeading ? subHeading : ""}
--- src/Pages/Home/ProductRec/ProductRec.jsx
+++ src/Pages/Home/ProductRec/ProductRec.jsx
@@ -1,19 +1,46 @@
-import React from 'react';
-import SectionTitle from '../../../Components/SectionTitle';
+import React from "react";
+import SectionTitle from "../../../Components/SectionTitle";
+
+// Slick slider style
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const ProductRec = () => {
-    return (
-        <section id='product-recomainded-wrapper'>
-            <div className="container">
-                <div className="product-recomainded-row">
-                    <SectionTitle heading={"CHEF RECOMMENDS"} subHeading={"---Should Try---"} />
-                    <div className="product-reco-card-wrapper">
-                        
-                    </div>
-                </div>
-            </div>
-        </section>
-    );
+  var setting = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
+  const [ChefMenu, setChefMenu] = useState([]);
+
+  useEffect(() => {
+    fetch("/menu.json")
+      .then((res) => res.json())
+      .then((data) => {
+        const popularMenu = data.filter((item) => item.category == "popular");
+        setChefMenu(popularMenu);
+      });
+  }, []);
+
+  return (
+    <section id="product-recomainded-wrapper">
+      <div className="container section-padding">
+        <div className="product-recomainded-row">
+          <SectionTitle
+            heading={"CHEF RECOMMENDS"}
+            subHeading={"---Should Try---"}
+          />
+          <div className="product-reco-card-wrapper">
+            
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default ProductRec;
